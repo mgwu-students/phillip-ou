20483c99bcf419f76277560d3373b572992dc7d9
Phillip Ou <phillipou@Phillips-MacBook-Pro.local>
2014-06-28 23:25:43 -0700

    Initial Commit

---
+++ Rascal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rascal
+//
+//  Created by Phillip Ou on 6/28/14.
+//  Copyright (c) 2014 Philip Ou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rascal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rascal
+//
+//  Created by Phillip Ou on 6/28/14.
+//  Copyright (c) 2014 Philip Ou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
